884b91f5d43e94e2619ff2929d3659b067ae9d8f
FernandoCaires <fernando.caires10@gmail.com>
2018-06-28 03:14:29 -0300

    PrimeiroCommit

---
+++ SistemaNutricionista/SistemaNutricionista/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaNutricionista.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public int  Idade { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
---
+++ SistemaNutricionista/SistemaNutricionista/Models/ClienteMedidas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaNutricionista.Models
+{
+    [Table("Medidas")]
+    public class ClienteMedidas
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("ClienteID")]
+        public int ClienteID { get; set; }
+
+        public int Altura { get; set; }
+        public decimal Peso { get; set; }
+        public decimal IMC { get; set; }
+        public int Cintura { get; set; }
+        public int Abdomen { get; set; }
+        public decimal PorcentagemGordura { get; set; }
+        public decimal PorcentagemGorduraViceral { get; set; }
+        public int Coxa { get; set; }
+
+
+    }
+}
